64accf08480002575970ace538a87dfc9af48664
KarolinaFilipiuk <karolina.filipiuk@interia.com>
2022-07-21 21:30:27 +0200

    [RED] Added checkbox

--- src/App.test.js
+++ src/App.test.js
@@ -9,8 +9,16 @@ test('button has correct initial color', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 
   fireEvent.click(colorButton);
-
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
-
   expect(colorButton).toHaveTextContent('Change to red');
+});
+
+test('initial conditions', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
 });
