4b395aa171b869f00a6091b6971fbafee751c225
KarolinaFilipiuk <karolina.filipiuk@interia.com>
2022-07-21 21:07:33 +0200

    [RED] Added posibility of changing color

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -7,6 +7,10 @@ test('button has correct initial color', () => {
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
-});
 
-test('button turns blue when clicked', () => {});
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  expect(colorButton).toHaveTextContent('Change to red');
+});
