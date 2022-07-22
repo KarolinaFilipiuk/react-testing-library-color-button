bd0e923a021c7a979ece0bcb0495757717fa96e9
KarolinaFilipiuk <karolina.filipiuk@interia.com>
2022-07-22 14:37:34 +0200

    [REFACTOR] Exported values of colors to seperate file

---
+++ src/colors.js
@@ -0,0 +1,4 @@
+export const FIRST_COLOR = 'MediumVioletRed';
+export const FIRST_COLOR_WITH_SPACES = 'Medium Violet Red';
+export const SECOND_COLOR = 'MidnightBlue';
+export const SECOND_COLOR_WITH_SPACES = 'Midnight Blue';
